fea590132b376309e25546017b2125136d1b9ce0
UniqueRevanth45 <yerrarevanth9@gmail.com>
2025-12-29 09:44:29 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,25 @@
+integer = 10          
+float= 3.14         
+complex= 2 + 3j      
+text = "Hello, Python!" 
+bool= True          
+list= [1, 2, 3, 4]         
+tuple= (10, 20, 30)       
+range= range(1, 5)         
+dict= {
+    "name": "Revanth",
+    "age": 21,
+    "course": "Python"
+}                                  
+set= {1, 2, 3, 4}          
+frozen_set= frozenset([5, 6, 7])
+nothing = None                     
+print(type(integer))
+print(type(float))
+print(type(text))
+print(type(list))
+print(type(dict))
+print(type(set))
+print(type(frozen_set))
+print(type(nothing))
+
